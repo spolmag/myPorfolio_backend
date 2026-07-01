ccea4bbff159f2450343eafe1fe9e33a9546344d
spolmag <spolmag@gmail.com>
2026-07-01 20:13:15 +0700

    Edit frontend url

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,4 +1,15 @@
 export const corsOption = {
-  origin: ["http://localhost:5173"],
+  origin: (origin, callback) => {
+    // Allow local development testing and any Vercel app domain dynamically
+    if (
+      !origin ||
+      origin.startsWith("http://localhost:") ||
+      origin.endsWith("vercel.app")
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Blocked by premium metallic security CORS layer"));
+    }
+  },
   credentials: true,
 };
